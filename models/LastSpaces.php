34e1a5f7ee3dfde0df4ece1a21c6dc2eb4e265c8
Guillaume Ponty <g.ponty@3cie.fr>
2016-04-07 11:51:14 +0200

    Changement image space et affiche les espaces dont l'utilisateur est membre

--- models/LastSpaces.php
+++ models/LastSpaces.php
@@ -2,6 +2,9 @@
 
 namespace humhub\modules\lastspaces\models;
 
+use humhub\modules\space\models\Space;
+use humhub\modules\space\models\Membership;
+
 /**
  * @author Guillaume Ponty
  */
@@ -11,9 +14,16 @@ class LastSpaces extends \humhub\modules\space\models\Space
     public static function find()
     {
         $query = parent::find();
-        $query->where('visibility > 0');
+        $query->leftJoin('space_membership', 'space.id=space_membership.space_id AND space_membership.user_id=:userId', [':userId' => \Yii::$app->user->id]);
+        $query->andWhere([
+            '!=', 'space.visibility', Space::VISIBILITY_NONE,
+        ]);
+        $query->orWhere([
+            'space_membership.status' => Membership::STATUS_MEMBER,
+        ]);
         $query->orderBy('created_at DESC');
         return $query;
     }
+
 
 }
--- widgets/views/sidebar.php
+++ widgets/views/sidebar.php
@@ -18,22 +18,30 @@ humhub\modules\lastspaces\Assets::register($this);
         foreach ($spaces as $space) {
             ?>
 
-            <a href="<?php echo $space->getUrl(); ?>">
-                <img src="<?php echo $space->getProfileImage()->getUrl(); ?>"  class="img-rounded tt img_margin" height="40"
-                     width="40" alt="40x40" data-src="holder.js/40x40" style="width: 40px; height: 40px;" data-toggle="tooltip"
-                     data-placement="top" title=""
-                     data-original-title="<?php echo Html::encode($space->name); ?>">
-            </a>
+            <?php echo \humhub\modules\space\widgets\Image::widget([
+                'space' => $space,
+                'width' => 40,
+                'link' => true,
+                'htmlOptions' => [
+                    'style' => 'margin-bottom: 5px;',
+                ],
+                'linkOptions' => [
+                    'class' => 'tt',
+                    'data-toggle' => 'tooltip',
+                    'data-placement' => 'top',
+                    'title' => Html::encode($space->name),
+                ]
+            ]); ?>
 
             <?php
         }
 
         echo "<hr />";
 
-        echo Html::a(Yii::t('LastspacesModule.base', 'Get a list'), Url::to(['/directory/directory/spaces']), array(
-            'class' => 'btn btn-info'
-        ));
+        echo Html::a(Yii::t('LastspacesModule.base', 'Get a list'), array('/directory/directory/spaces'), array('class' => 'btn btn-xl btn-info'));
+
         ?>
+
     </div>
 </div>
 
